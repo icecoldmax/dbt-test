23155ccba25c044c26ad8ec5081ed45e64072f04
David Nettle <david.nettle@3plearning.com>
2023-05-15 12:14:17 +0900

    Try copying files in and configuring artifact path

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 set -eo pipefail
 
 echo "this is running from script.sh"
-echo "the result of running which dbt is:"
-echo "$(which dbt)"
+echo "the result of running which dbt is: `which dbt`"
+
+echo "copying target directory"
+cp -R ~/Code/target ~/Code/dbt-test/target
+echo "copying done"
