ef15583bfc2199e48516faad2e3e05e5e5615f27
David Nettle <david.nettle@3plearning.com>
2023-05-15 14:07:04 +0900

    Try inlining with jaffle_shop docs

--- script.sh
+++ script.sh
@@ -8,3 +8,6 @@ echo current working directory is `pwd`
 echo "copying target directory"
 cp -R ~/Code/target target
 echo "copying done"
+echo "inlining json into index-inline.html"
+python inline.py
+echo "json inlined"
