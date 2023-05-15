cbeff84c9e187fc4dfda89bac6cadf59461547f9
David Nettle <david.nettle@3plearning.com>
2023-05-15 12:20:54 +0900

    Tweaks to pipeline and script

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 echo "this is running from script.sh"
 echo "the result of running which dbt is: `which dbt`"
 
+echo current working directory is `pwd`
+
 echo "copying target directory"
 cp -R ~/Code/target ~/Code/dbt-test/target
 echo "copying done"
